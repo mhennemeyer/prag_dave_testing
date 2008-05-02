489dd62b59a14447bebd107f723d3e1e29b797de
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-05-02 13:32:31 +0200

    Turned the initial examples by prag_dave into tests.

--- test/test_tmc.rb
+++ test/test_tmc.rb
@@ -1,16 +1,86 @@
 require 'test_helper'
 
-testing "negated comparison with !=" do
+
+testing "Regular tests" do
   
-  result = run { expect(1) != 1 }
-  expect(result) =~ /but\s1\s==\s1/
-  
-  result = run {expect("Horst") !~ /Horst/}
-  expect(result) =~ /but\s\"Horst\"\s=~\s\/Horst\//
-  
-  result = run {
-    expect("Horst") =~ /Horst/ # HERE IS A BAD != 
-  }
-  expect(result) == ""
+  expect(1) == 1 
+  expect(1) < 3
+  expect("cat") =~ /[aeiou]/
   
 end
+  
+
+testing "groups of tests" do
+  
+  testing("negative numbers") do
+    expect(-3) <= -3
+    expect(-1) > -1000
+    testing("negative floating point numbers") do
+      expect(-3.0) <= -3
+    end
+  end
+  
+end
+
+testing "Transactional instance variables" do
+  
+  @var = "cat"
+  expect(@var) =~ /a/
+  testing("uppercase version") do
+    @var.upcase!
+    expect(@var) =~ /A/
+    testing("reversed") do 
+      @var = @var.reverse
+      expect(@var) == "TAC"
+    end
+    expect(@var) == "CAT"
+  end
+  expect(@var) =~ /a/  # original value restored
+  
+end
+
+testing "annotation of failed tests" do
+
+  result = run {
+    # this comment will annotate the following failed test
+    expect(1) == 2
+  }
+
+  expect(result) =~ /this\scomment\swill\sannotate\sthe\sfollowing\sfailed\stest/
+
+  result = run { 
+    expect(2) == 3  # so will this one
+  }
+  
+  expect(result) =~ /so\swill\sthis\sone/
+  
+end
+
+
+
+testing "negated comparison" do 
+
+  testing "with !=" do
+  
+    result = run { expect(1) != 1 }
+    expect(result) =~ /but\s1\s==\s1/
+
+  end
+
+  testing "with !~" do
+  
+    result = run { expect("Horst") !~ /Horst/ }
+    expect(result) =~ /but\s\"Horst\"\s=~\s\/Horst\//
+  
+  end
+
+  testing "should ignore negations in comments" do
+  
+    result = run {
+      expect("Horst") =~ /Horst/ # HERE IS A BAD != 
+    }
+    expect(result) == ""
+  
+  end
+end
+
