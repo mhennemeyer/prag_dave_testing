55f205877061a48442965b08328c365942e719a7
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-05-03 15:47:20 +0200

    Added some more tests for this setup thing.

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -18,14 +18,31 @@ testing "var should not eql 2" do
   end
 end
 
+
+# Try a deep nesting
+
 testing "with a second setup" do
   setup { @var = 3 }
   
   testing "var should eql 3" do
     expect(@var) == 3
+    testing "nothing again" do
+      expect(1) == 1
+      testing "one more level" do
+        expect(12) < 13
+        testing "and more" do 
+          setup { @var = 12 }
+          testing "var should eql 12" do
+            expect(@var) == 12
+          end
+        end
+      end
+    end
   end
   
   # running the named testing block again shouldn't trigger the setup
   @var = 1
   expect_var_should_eql_1
+  @var = 12
+  expect_var_should_eql_12
 end
